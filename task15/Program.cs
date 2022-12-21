bd846c4683f71c577c93ca40f40dfb5ede8114c9
lazutkinkirill <lazutkin_k@inbox.ru>
2022-12-21 23:06:19 +0300

    finish task 15

---
+++ task15/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+ 
+ Console.Write("Введи день недели цифрой: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+void Check (int number) 
+{
+
+  if (number == 6 || number == 7) 
+  {
+  Console.WriteLine("выходной!!!");
+  }
+  
+  else if (number < 1 || number > 7) 
+  {
+    Console.WriteLine("что - то на эльфийском");
+  }
+
+  else Console.WriteLine("этот день не выходной, работать, кожаный");
+
+}
+
+
+
+
+Check(number);

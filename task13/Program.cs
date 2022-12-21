268aff420f18e9b3308dc219dc65a327ad018272
lazutkinkirill <lazutkin_k@inbox.ru>
2022-12-21 22:48:16 +0300

    finish task 13

---
+++ task13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+    int number = 12;
+    string numberText = Convert.ToString(number);
+        if (numberText.Length > 2)
+        {
+        Console.WriteLine("third number is " + numberText[2]);
+        }
+        else 
+        {
+            Console.WriteLine("there is no third number");
+        }

220d6595fc9cac2070a292b606228951b08ad3cb
lazutkinkirill <lazutkin_k@inbox.ru>
2022-12-21 22:24:23 +0300

    finish task 10

---
+++ task10/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Напишите программу, которая 
+принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа */
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+
+        string number = 457.ToString();
+        Console.WriteLine(number[1]);
+    }
+}
